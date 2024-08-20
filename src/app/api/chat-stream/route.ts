c21c5f4f8d38c1ea33739fefc607f66aa41af49c
Vedant Mahajan <vedantmahajan271@gmail.com>
2024-08-20 11:06:39 +0530

    Chat Stream Route

---
+++ src/app/api/chat-stream/route.ts
@@ -0,0 +1,13 @@
+import { ragChat } from "@/lib/rag-chat";
+import { aiUseChatAdapter } from "@upstash/rag-chat/nextjs";
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = async(req: NextRequest, res: NextResponse) => {
+    const {messages, sessionId} = await req.json();
+
+    const lastMessage = messages[messages.length - 1].content;
+    const response = await ragChat.chat(lastMessage, {sessionId, streaming: true});
+    console.log(response);
+
+    return aiUseChatAdapter(response);
+}
